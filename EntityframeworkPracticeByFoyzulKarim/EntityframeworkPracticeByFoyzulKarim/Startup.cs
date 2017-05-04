e7da2984c2c2f87a316523d8f5b5e22afddfbcb5
Plabqn Modak - Pinku <plaboncsejnu21@gmail.com>
2017-05-04 12:24:35 +0600

    InitialCommit

---
+++ EntityframeworkPracticeByFoyzulKarim/EntityframeworkPracticeByFoyzulKarim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityframeworkPracticeByFoyzulKarim.Startup))]
+namespace EntityframeworkPracticeByFoyzulKarim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
